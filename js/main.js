a39eaa47194df0a109656a3d098c566d8e0bbc94
Danny Michaux <dannymichaux@gmail.com>
2018-05-14 20:51:13 +0100

    added pictures

--- js/main.js
+++ js/main.js
@@ -4,9 +4,22 @@ SVG.on(document, 'DOMContentLoaded', function() {
   if (SVG.supported) {
   var draw = SVG('drawing').size(1000,1000)
 
-  var pattern = draw.pattern(1000, 1000, function(add) {
-  add.image('../images/great-wave-image-1600-1.jpg').size(1000, 1000)
-})
+  var patternOne = draw.pattern(1000, 1000, function(add) {
+  add.image('https://picsum.photos/1000/1000?image=1084').size(1000, 1000)
+  })
+
+  var patternTwo = draw.pattern(1000, 1000, function(add) {
+  add.image('https://picsum.photos/1000/1000?image=1083').size(1000, 1000)
+  })
+
+  var patternThree = draw.pattern(1000, 1000, function(add) {
+  add.image('https://picsum.photos/1000/1000?image=1082').size(1000, 1000)
+  })
+
+  var patternFour = draw.pattern(1000, 1000, function(add) {
+  add.image('https://picsum.photos/1000/1000?image=1081').size(1000, 1000)
+  })
+
 
   var xOne = (Math.round((Math.random() * 800))) + 100
   var yOne = (Math.round((Math.random() * 800))) + 100
@@ -18,9 +31,9 @@ SVG.on(document, 'DOMContentLoaded', function() {
   var yFour = (Math.round((Math.random() * 800))) + 100
 
   var circleOne = draw.circle(0).fill('url(#SvgjsPattern1008)').stroke('black').attr({'stroke-width' : 5}).move(xOne, yOne).animate(300, '>', 0).radius(100).animate(300, '>', 0).radius(95).loop(1).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(xOne, yOne).loop()
-  var circleTwo = draw.circle(0).fill('transparent').stroke('black').attr({'stroke-width' : 5}).move(xTwo, yTwo).animate(300, '>', 0).radius(100).animate(300, '>', 0).radius(95).loop(1).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(xTwo, yTwo).loop()
-  var circleThree = draw.circle(0).fill('transparent').stroke('black').attr({'stroke-width' : 5}).move(xThree, yThree).animate(300, '>', 0).radius(100).animate(300, '>', 0).radius(95).loop(1).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(xThree, yThree).loop()
-  var circleFour = draw.circle(0).fill('transparent').stroke('black').attr({'stroke-width' : 5}).move(xFour, yFour).animate(300, '>', 0).radius(100).animate(300, '>', 0).radius(95).loop(1).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(xFour, yFour).loop()
+  var circleTwo = draw.circle(0).fill('url(#SvgjsPattern1010)').stroke('black').attr({'stroke-width' : 5}).move(xTwo, yTwo).animate(300, '>', 0).radius(100).animate(300, '>', 0).radius(95).loop(1).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(xTwo, yTwo).loop()
+  var circleThree = draw.circle(0).fill('url(#SvgjsPattern1012)').stroke('black').attr({'stroke-width' : 5}).move(xThree, yThree).animate(300, '>', 0).radius(100).animate(300, '>', 0).radius(95).loop(1).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(xThree, yThree).loop()
+  var circleFour = draw.circle(0).fill('url(#SvgjsPattern1014)').stroke('black').attr({'stroke-width' : 5}).move(xFour, yFour).animate(300, '>', 0).radius(100).animate(300, '>', 0).radius(95).loop(1).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(Math.round((Math.random() * 800)), Math.round((Math.random() * 800))).animate(10000, '<>', 0).move(xFour, yFour).loop()
 
 
 
