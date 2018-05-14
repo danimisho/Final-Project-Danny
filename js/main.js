002aad9109d821368b0f1ed867831de2247117e7
Danny Michaux <dannymichaux@gmail.com>
2018-05-14 14:22:43 +0100

    added svg.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 console.log('it works');
 
-$(document).ready(function() {
-
-  function grow(){
-    $(div)
-  }
-
+SVG.on(document, 'DOMContentLoaded', function() {
+  if (SVG.supported) {
+  var draw = SVG('drawing').size(1000,1000)
+  var rect = draw.rect(300, 300)
+} else {
+  alert('SVG not supported')
+}
 })
